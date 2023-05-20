3c49178f5556ad55ff08963b7d2ce8711391d4d4
reza107-hub <rashidaminur104@gmail.com>
2023-05-20 22:28:26 +0600

    final commit

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ const port = process.env.PORT || 5000
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
-
+app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 
