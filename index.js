8892fb4fdc820637841876e368f9a516ddd8dc14
reza107-hub <rashidaminur104@gmail.com>
2023-05-21 17:30:13 +0600

    in vercel.json methods put added

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.get('/', (req, res) => {
 })
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfxumrl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
